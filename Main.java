4ac8301cd79bb982251f44dbc2308e50a1b5145e
PAVAN-CBIT <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-08-02 18:21:48 +0530

    Add files via upload
    Printing odd and even numbers

---
+++ Main.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int n=100;
+		int i;
+		for(i=0;i<=n;i++){
+		    if(i%2==0){
+		        System.out.println(i);
+		    }
+		}
+	}
+}
