d715a42d1339ec7657cc3ab4be2139531e60d201
PAVAN-CBIT <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-08-06 19:51:06 +0530

    Create factiterative.java
    Factiterative

---
+++ factiterative.java
@@ -0,0 +1,16 @@
+public class Factiterative
+{
+	public static void main(String[] args)
+	{
+	    
+		System.out.println("Hello World");
+		int m,n,i;
+		m=1;
+		n=5;
+		for(i=1;i<=n;i++)
+		{
+		    m=m*i;
+		}
+		System.out.println(m);
+	}
+}
