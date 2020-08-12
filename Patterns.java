f447ada0460085d50d3db9182cdf679f9d7fc6cf
PAVAN-CBIT <69111120+PAVAN-CBIT@users.noreply.github.com>
2020-08-12 18:48:54 +0530

    Create Patterns.java
    Patterns.java

---
+++ Patterns.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args)
+	{
+	System.out.println("Hello World");
+	int i;
+	int j;
+	int n=10;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            System.out.print(j);
+        }
+        System.out.println("");
+    }
+    
+    
+}
+}
