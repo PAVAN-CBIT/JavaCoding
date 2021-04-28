6a52e0552252abee384eaaac4213d92144c77a32
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-28 16:42:25 +0530

    Create While_loops_basic_Programme-ve value.java

---
+++ While_loops_basic_Programme-ve value.java
@@ -0,0 +1,18 @@
+public class While loop
+{
+
+     
+     public static void main(String []args)
+       
+     
+     {
+        String str ="hello kp";
+		   int a=str.indexOf("world");
+		
+       {
+		     System.out.println(a);
+       }
+  
+     
+     }
+}  
