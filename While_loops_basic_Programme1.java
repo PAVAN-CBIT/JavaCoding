e05fa94e2465fcfe5880bc7b7c1466e1814150b0
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-28 09:48:44 +0530

    Create While_loops_basic_Programme1.java

---
+++ While_loops_basic_Programme1.java
@@ -0,0 +1,18 @@
+public class Whileloop
+{
+	public static void main(String[] args) {
+		
+		int c=0;
+		while(c<=100)
+		{
+		    
+		    System.out.println("Hello Pavan"+c);
+		    c=c+1;
+		    if(c==30)
+		    {
+		        break;
+		    }
+		    
+		}
+	}
+}
