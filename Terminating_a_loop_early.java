28650c57f8344eb8a08d9f2b6d224c7bb45ebb8b
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-04-28 16:34:03 +0530

    Create Terminating_a_loop_early.java

---
+++ Terminating_a_loop_early.java
@@ -0,0 +1,15 @@
+Public class Whileloop
+{
+public static void main(String args[])
+{
+
+    int c =0;
+		while(c<=100)
+		{
+		    System.out.println("Hello :"+c);
+		    break;
+      
+    }
+}
+  
+}
